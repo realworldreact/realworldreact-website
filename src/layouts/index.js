ff513be9d7ce04ac764a8703548a913ba8f5e7c4
Romel Pérez <ronelprhone@gmail.com>
2018-02-22 00:41:35 -0500

    update: layours, base styles and assets

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { ThemeProvider } from 'react-jss';
 
-import './styles/normalize.css';
+import './styles/normalize.min.css';
+import './styles/flexboxgrid.min.css';
 import './styles/fonts.css';
 import './styles/global.css';
+
 import favicon from '../assets/favicon.ico';
 import createTheme from './createTheme';
 import ThemedStyles from './ThemedStyles';
@@ -14,6 +16,8 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet>
       <link rel='shortcut icon' href={favicon} />
+      <link rel='stylesheet' href='//cdn.materialdesignicons.com/2.1.19/css/materialdesignicons.min.css' />
+      <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Neuton:400,400i' />
     </Helmet>
     <ThemeProvider theme={createTheme()}>
       <ThemedStyles>
