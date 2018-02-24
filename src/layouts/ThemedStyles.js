d6cae45d996a760efad85f5892482a26c2519125
Romel Pérez <ronelprhone@gmail.com>
2018-02-23 23:51:29 -0500

    feat: add text and background utility classes

--- src/layouts/ThemedStyles.js
+++ src/layouts/ThemedStyles.js
@@ -14,6 +14,8 @@ const styles = theme => ({
         color: theme.palette.primary.light
       }
     },
+
+    // Global selection
     '::selection': {
       backgroundColor: theme.palette.primary.base,
       color: theme.palette.primary.contrastText
@@ -22,8 +24,25 @@ const styles = theme => ({
       backgroundColor: theme.palette.primary.base,
       color: theme.palette.primary.contrastText
     },
+
+    // Text
     '.text-primary': {
       color: theme.palette.primary.base
+    },
+    '.text-secondary': {
+      color: theme.palette.secondary.base
+    },
+    '.text-inverted': {
+      color: theme.palette.inverted.base
+    },
+    '.background-primary': {
+      backgroundColor: theme.palette.primary.base
+    },
+    '.background-secondary': {
+      backgroundColor: theme.palette.secondary.base
+    },
+    '.background-inverted': {
+      backgroundColor: theme.palette.inverted.base
     }
   }
 });
